30623ac8ce600e3a24d1534aed1f25db67e8fc69
festus <kiamfestoh@gmail.com>
2019-05-03 11:36:06 +0300

    add signup user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :messages, except: [:update, :show]
   post 'auth/login', to: 'authentication#authenticate'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'signup', to: 'users#create'
 end
